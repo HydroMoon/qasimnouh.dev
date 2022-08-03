aae854ce6cc52bf35d612ead5e06d7b851155a34
Qasim Nouh <vs.dev50@gmail.com>
2022-08-04 00:26:37 +0300

    [FIX] favicon

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,7 +8,9 @@
     </header>
     <slot />
     <footer class="flex justify-center pb-4 mt-4 sm:mt-0">
-      <span class="text-sm text-gray-200">Made with <span class="text-red-700">♥</span> by Qasim</span>
+      <span class="text-sm text-gray-200"
+        >Made with <span class="text-red-700">♥</span> by Qasim</span
+      >
     </footer>
   </div>
 </template>
@@ -18,7 +20,30 @@ useHead({
   viewport: "width=device-width, initial-scale=1, maximum-scale=1",
   charset: "utf-8",
   meta: [{ name: "description", content: "Qasim Nouh personel website" }],
-  link: [{ rel: "shorcut icon", href: "/favicon.ico" }],
+  link: [
+    {
+      rel: "shortcut icon",
+      href: "/favicon.ico",
+    },
+    {
+      rel: "apple-touch-icon",
+      sizes: "180x180",
+      href: "/apple-touch-icon.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "32x32",
+      href: "/favicon-32x32.png",
+    },
+    {
+      rel: "icon",
+      type: "image/png",
+      sizes: "16x16",
+      href: "/favicon-16x16.png",
+    },
+    { rel: "manifest", href: "/site.webmanifest" },
+  ],
   bodyAttrs: {
     class: "bg-slate-900 h-full font-[Helvetica]",
   },
