cea95e8107568f6eaf045cb0defbe4b1fe4958dc
Qasim <vs.dev50@gmail.com>
2023-11-05 10:40:28 +0300

    [ADD] base url for gh pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,10 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  app: {
+    baseURL: '',
+  },
   typescript: {
-    strict: true
+    strict: true,
   },
   modules: ['@nuxt/content', '@nuxtjs/tailwindcss', 'nuxt-headlessui'],
-});
+})
